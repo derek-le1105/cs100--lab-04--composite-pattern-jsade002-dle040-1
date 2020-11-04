88070ea2eee47741ad56f60cc65cd4f6488f47d9
derek-le1105 <derekle1105@gmail.com>
2020-11-03 16:20:35 -0800

    Added mult class unit tests

---
+++ mult_test.hpp
@@ -0,0 +1,29 @@
+#ifndef __MULT_TEST_HPP__
+#define __MULT_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "Mult.hpp"
+
+TEST(MultTest, MultEvaluate2Num) {
+    Op* leftTest = new Op(7);
+    Op* rightTest = new Op(8);
+    Mult* test = new Mult(leftTest, rightTest);
+    EXPECT_EQ(test->evaluate(), 56);   
+}
+
+TEST(MultTest, MultEvaluateNumWithZero) {
+    Op* leftTest = new Op(0);
+    Op* rightTest = new Op(5);
+    Mult* test = new Mult(leftTest, rightTest);
+    EXPECT_EQ(test->evaluate(), 0);
+}
+
+TEST(MultTest, MultEvaluateNegativeNums) {
+    Op* leftTest = new Op(-1);
+    Op* rightTest = new Op(9);
+    Mult* test = new Mult(leftTest, rightTest);
+    EXPECT_EQ(test->evaluate(), -9);
+}
+
+#endif //__MULT_TEST_HPP__

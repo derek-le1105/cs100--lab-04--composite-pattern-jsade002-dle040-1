9d45f6ac005eac6fc08357571869db6db2656e4a
derek-le1105 <derekle1105@gmail.com>
2020-11-03 17:10:20 -0800

    Added add operation and add unit test

---
+++ add_test.hpp
@@ -0,0 +1,38 @@
+#ifndef __ADD_TEST_HPP__
+#define __ADD_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "Add.hpp"
+
+TEST(AddTest, AddEvaluate2Num) {
+    Op* leftTest = new Op(7);
+    Op* rightTest = new Op(8);
+    Add* test = new Add(leftTest, rightTest);
+    EXPECT_EQ(test->evaluate(), 15);   
+}
+
+TEST(AddTest, AddEvaluateNumWithZero) {
+    Op* leftTest = new Op(0);
+    Op* rightTest = new Op(5);
+    Add* test = new Add(leftTest, rightTest);
+    EXPECT_EQ(test->evaluate(), 5);
+}
+
+TEST(AddTest, AddEvaluateNegativeNums) {
+    Op* leftTest = new Op(-1);
+    Op* rightTest = new Op(9);
+    Add* test = new Add(leftTest, rightTest);
+    EXPECT_EQ(test->evaluate(), 8);
+}
+
+TEST(AddTest, AddEvaluateWithMult) {
+    Op* leftTest = new Op(5);
+    Op* LHS = new Op(2);
+    Op* RHS = new Op(3);
+    Mult* rightTest = new Mult(LHS, RHS);
+    Add* test = new Add(leftTest, rightTest);
+    EXPECT_EQ(test->evaluate(), 11);
+}
+
+#endif //__ADD_TEST_HPP__
